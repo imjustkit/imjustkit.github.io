f874eeddb1739ab10392708bf2ea0d065712b350
krystyn <127636988+imjustkit@users.noreply.github.com>
2025-05-15 22:26:42 +0100

    bwa

--- js/clicker.js
+++ js/clicker.js
@@ -2,11 +2,7 @@ var clicks = 0
 var clickpower = 1
 var clickmulti = 1
 
-function update() {
-    document.getElementById('clickCount').innerHTML = clicks.toPrecision()
-}
-
 function clicker() {
     clicks += clickpower * clickmulti
-    update()
+    document.getElementById('clickCount').innerHTML = clicks.toPrecision()
 }
